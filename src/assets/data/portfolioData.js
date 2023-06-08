9973243da520785893e23ed3269f5d1ed1ad579b
Widraaa <ardiwidiyanto12@gmail.com>
2023-06-08 13:49:47 +0700

    update portos

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,7 +1,7 @@
 import portfolioImg01 from "../images/portfolio-01.jpg";
-// import portfolioImg02 from "../images/portfolio-02.jpg";
-// import portfolioImg03 from "../images/portfolio-03.jpg";
-// import portfolioImg04 from "../images/portfolio-04.jpg";
+import portfolioImg02 from "../images/portfolio-02.jpg";
+import portfolioImg03 from "../images/portfolio-03.jpg";
+import portfolioImg04 from "../images/portfolio-04.jpg";
 // import portfolioImg05 from "../images/portfolio-05.jpg";
 // import portfolioImg06 from "../images/portfolio-06.jpg";
 // import portfolioImg07 from "../images/portfolio-07.jpg";
@@ -10,33 +10,33 @@ const portfolios = [
   {
     id: "01",
     imgUrl: portfolioImg01,
-    category: "Apps Design",
+    category: "Web Design",
     title: "Finance Technology Apps",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-    technologies: ["Dart", "Tailwind css"],
+    technologies: ["Dart"],
     siteUrl: "#",
   },
-  // {
-  //   id: "02",
-  //   imgUrl: portfolioImg04,
-  //   category: "Ux",
-  //   title: "Video Conference Website",
-  //   description:
-  //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-  //   technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-  //   siteUrl: "#",
-  // },
-  // {
-  //   id: "03",
-  //   imgUrl: portfolioImg02,
-  //   category: "Ux",
-  //   title: "Multimedia Sharing Website",
-  //   description:
-  //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-  //   technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-  //   siteUrl: "#",
-  // },
+  {
+    id: "02",
+    imgUrl: portfolioImg04,
+    category: "Ux",
+    title: "Video Conference Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "03",
+    imgUrl: portfolioImg02,
+    category: "Ux",
+    title: "To-do List",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["Figma"],
+    siteUrl: "#",
+  },
   // {
   //   id: "04",
   //   imgUrl: portfolioImg05,
@@ -47,16 +47,16 @@ const portfolios = [
   //   technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
   //   siteUrl: "#",
   // },
-  // {
-  //   id: "05",
-  //   imgUrl: portfolioImg03,
-  //   category: "Web Design",
-  //   title: "Landing Page",
-  //   description:
-  //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
-  //   technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
-  //   siteUrl: "#",
-  // },
+  {
+    id: "05",
+    imgUrl: portfolioImg03,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["HTML", "CSS", "Javascript"],
+    siteUrl: "#",
+  },
 
   // {
   //   id: "06",
